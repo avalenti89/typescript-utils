7abab4c4aaaaee4c58f422b433c32fc7b83b37c1
Antonio Valenti <valentiantonio89@gmail.com>
2021-05-10 15:53:13 +0200

    removes external types

--- src/types/RecorderStatus.ts
+++
@@ -1,8 +0,0 @@
-export enum RecorderStatus {
-	IDLE = 'idle',
-	INVALID_CONSTRAINTS = 'invalid_constraints',
-	RECORDING = 'recording',
-	STOPPED = 'stopped',
-	ERROR = 'error',
-	NOT_SUPPORTED = 'not_supported'
-}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,2 @@
 export * from './KeyString';
 export * from './PickPartial';
-export * from './RecorderStatus';
-export * from './Size';
