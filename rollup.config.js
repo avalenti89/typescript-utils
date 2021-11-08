1350b7686a8bb00f9c5ffedc9b4b7166bba372a4
avalenti89 <valentiantonio89@gmail.com>
2021-11-08 16:31:57 +0100

    removes tsdx and use rollup

---
+++ rollup.config.js
@@ -0,0 +1,49 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import typescript from '@rollup/plugin-typescript';
+import packageJson from './package.json';
+import json from '@rollup/plugin-json';
+import babel from '@rollup/plugin-babel';
+import includePaths from 'rollup-plugin-includepaths';
+
+let includePathOptions = {
+	include: {},
+	paths: ['src'],
+	external: [],
+	extensions: ['.ts']
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+	input: './src/index.ts',
+	output: [
+		{
+			file: packageJson.main,
+			format: 'cjs', // commonJS
+			sourcemap: true
+		},
+		{
+			file: packageJson.module,
+			format: 'esm',
+			sourcemap: true
+		}
+	],
+	preserveSymlinks: true,
+	external: {},
+	plugins: [
+		peerDepsExternal(),
+		resolve({ jsnext: true, main: true }),
+		commonjs({
+			include: /node_modules/
+		}),
+		babel({
+			exclude: /node_modules/,
+			babelHelpers: 'runtime'
+		}),
+		typescript({ tsconfig: './tsconfig.json' }),
+
+		json(),
+		includePaths(includePathOptions)
+	]
+};
