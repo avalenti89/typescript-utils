5da10f756c049aab8b520c68e65de2cba3b86132
Antonio Valenti <valentiantonio89@gmail.com>
2022-01-19 17:35:53 +0100

    adds WithListener class

---
+++ src/classes/WithListener.ts
@@ -0,0 +1,22 @@
+export class WithListener<T extends string, E = any> {
+	listeners: Partial<Record<T, Array<(data: E) => void>>> = {};
+	on = <C extends E>(eventType: T, callback: (event: C) => void) => {
+		this.listeners = {
+			...this.listeners,
+			[eventType]: [...(this.listeners[eventType] ?? []), callback],
+		};
+		return () => {
+			this.listeners = {
+				...this.listeners,
+				[eventType]: this.listeners[eventType]?.filter(
+					(listener) => listener !== callback
+				),
+			};
+		};
+	};
+	notify = (eventType: E, data: E) => {
+		this.listeners[eventType]?.forEach((callback) => {
+			callback(data);
+		});
+	};
+}
